8f17fd14ad3e1b2650591e4a69c2480b9c0af159
jhon1108 <jhon11082006.2@gmail.com>
2025-10-21 21:26:20 -0500

    Se agrega el proyecto completo

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
